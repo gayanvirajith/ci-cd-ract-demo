4f742a17cf90290753758182dc3cf3fc3e78c0ba
Gayan Virajith <gayanvirajith@gmail.com>
2020-09-01 01:46:20 +0530

    appspec

--- codedeploy/app_start.sh
+++ codedeploy/app_start.sh
@@ -3,3 +3,4 @@
 # This script is used to start the application
 sudo cp -r /usr/app/* /usr/share/nginx/html/
 sudo service nginx reload
+sudo rm -rf /usr/share/nginx/html/codedeploy /usr/share/nginx/html/appspec.yml
